a81747af4f61c4b20c19b9946763a9c733a3e623
hwp9527 <hwp195@163.com>
2018-04-18 01:12:03 +0800

    1、添加shadowsocks.json模板 2、完善deploy脚本

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,18 @@ function SystemType(){
     fi
 }
 
+function IsCmdInstalled(){
+    if [ $# -lt 1 ]; then
+	echo "IsCmdInstalled: parameters -lt 1"
+	exit 255
+    fi  
+
+    [ `which $1` ] && echo "$1: installed" && return 0
+
+    echo "$1: Not be installed"
+    return 1
+}
+
 sysType=`SystemType`
 echo -e "\033[32mCurrent System Type \033[00m<\033[31m${sysType}\033[00m>"
 if [ ${sysType} == "Ubuntu" ]; then
@@ -40,3 +52,14 @@ cat ./mybashrc.cfg >> ~/.bashrc
 sudo mkdir -p /usr/share/stardict/dic
 sudo tar jxvf stardict.tar.bz2 -C /usr/share/stardict/dic
 
+#Git configuration
+if [ `IsCmdInstalled git` -eq 0 ];then
+    git config --global push.default simples
+    git config --global user.name "hwp9527"
+    git config --global user.email hwp195@163.com
+    git config --global core.editor vim
+    git config --global diff.tool vimdiff
+fi
+
+#Go doc std chinese version
+git clone git@github.com:polaris1119/pkgdoc.git ~/pkgdoc
